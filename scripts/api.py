891d58f720f535bf762d5917fb6d6e7b9e72fded
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-02-19 14:05:20 +0100

    Refactor API request models and enhance filtering logic
    This commit improves the structure of the API request models by
    reordering attributes to enhance readability and consistency.
    Specifically, the `query` attribute in `SearchRequest` was moved to
    follow the `limit` attribute, while the `address` in `VerseRequest` was
    repositioned for better clarity. Additionally, new filtering methods
    were introduced to streamline the condition checks for searching verses,
    making the code more maintainable. The changes ensure the API can handle
    requests more flexibly, particularly by processing the `address` input
    properly through the new `to_address` method, enhancing how quotes are
    matched against the database. These modifications better align with the
    requirements outlined in the user story focused on improving the search
    functionality of the DeepBible API.

--- scripts/api.py
+++ scripts/api.py
@@ -4,17 +4,18 @@ from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
 from qdrant_client.models import Filter, FieldCondition, MatchValue
 import os
+import re
 
 class SearchRequest(BaseModel):
-    query: str
     limit: int = 50
+    query: str
     category: str = None
     source: str = None
 
 class VerseRequest(BaseModel):
-    address: str
     limit: int = 50
-    source: str = "pau"
+    address: str
+    source: str = None
 
 class DeepBibleAPI:
     def __init__(self, model_name=None, collection_name=None, qdrant_url=None, batch_size=None, device=None, target_dir=None):
@@ -27,51 +28,56 @@ class DeepBibleAPI:
         self.app = FastAPI()
         self.setup_routes()
 
-    def sqlite_path(self, db):
-        return os.path.join(self.target_dir, f"{db}.SQLite3")
+    def match_category(self, text):
+        return FieldCondition(key="category", match=MatchValue(value=text))
+
+    def match_source(self, text):
+        return FieldCondition(key="source", match=MatchValue(value=text))
+
+    def match_reference(self, text):
+        return FieldCondition(key="reference", match=MatchValue(value=text))
+
+    def to_address(self, text):
+        pattern = r'(\b\w+).*(\d+)[^\d]+(\d+)' # Matches book, chapter, verse
+        match = re.match(pattern, text)
+        if match:
+            book, chapter, verse = match.groups()
+            return f"{book.capitalize()} {chapter}.{verse}"
+        else:
+            return text
 
     def setup_routes(self):
         # Fetch verses from database
         @self.app.post("/verses")
         async def verses(request: VerseRequest):
-            filters = Filter(must=[FieldCondition(key="category",
-                                         match=MatchValue(value="textus")),
-                          FieldCondition(key="source",
-                                         match=MatchValue(value=request.source)),
-                          FieldCondition(key="reference",
-                                         match=MatchValue(value=request.address))])
-            search_results = self.client.scroll(
-                collection_name=self.collection_name,
-                scroll_filter=filters,
-                limit=request.limit
-            )
+            address = self.to_address(request.address)
+            conditions = [self.match_category("textus"),
+                          self.match_reference(address)]
+            if request.source:
+                conditions.append(self.match_source(request.source))
+            filters = Filter(must=conditions)
+            search_results = self.client.scroll(collection_name=self.collection_name, scroll_filter=filters, limit=request.limit)
             results = [ {**res.payload} for res in search_results[0] ]
-            return {"address": request.address, "results": results}
+            return {"address": address, "results": results}
 
         # Find similar verses
         @self.app.post("/similar_verses")
-        def get_similar_verses(request: VerseRequest):
-            results = self.client.scroll(
-                collection_name=self.collection_name,
-                scroll_filter=Filter(must=[FieldCondition(key="reference",
-                                                          match=MatchValue(value=request.address)),
-                                           FieldCondition(key="source",
-                                                          match=MatchValue(value=request.source))
-                                           ]),
-                limit=1
-            )
-            query_embedding = self.model.encode([results[0][0].payload["text"]])[0]
-            search_results = self.client.search(
-                collection_name=self.collection_name,
-                query_vector=query_embedding,
-                limit=request.limit,
-                query_filter=Filter(must=[FieldCondition(key="category",
-                                                         match=MatchValue(value="textus")),
-                                          FieldCondition(key="source",
-                                                         match=MatchValue(value=request.source))])
-            )
+        def similar_verses(request: VerseRequest):
+            address = self.to_address(request.address)
+            if not request.source:
+                request.source = "pau"
+            conditions = [self.match_category("textus"),
+                          self.match_source(request.source),
+                          self.match_reference(address)]
+            filters = Filter(must=conditions)
+            result = self.client.scroll(collection_name=self.collection_name, scroll_filter=filters, limit=1)[0][0].payload["text"]
+            query_embedding = self.model.encode([result])[0]
+            conditions = [self.match_category("textus"),
+                          self.match_source(request.source)]
+            filters = Filter(must=conditions)
+            search_results = self.client.search(collection_name=self.collection_name, query_vector=query_embedding, limit=request.limit, query_filter=filters)
             results = [ {**res.payload, "score": res.score} for res in search_results ]
-            return {"address": request.address, "results": results}
+            return {"address": address, "results": results}
 
         # Search by vector proximity
         @self.app.post("/search")
@@ -79,13 +85,9 @@ class DeepBibleAPI:
             query_embedding = self.model.encode([request.query])[0]
             conditions = []
             if request.category:
-                conditions.append(
-                    FieldCondition(key="category", match=MatchValue(value=request.category))
-                )
+                conditions.append(self.match_category(request.category))
             if request.source:
-                conditions.append(
-                    FieldCondition(key="source", match=MatchValue(value=request.source))
-                )
+                conditions.append(self.match_source(request.source))
             filters = Filter(must=conditions) if conditions else None
             search_results = self.client.search(
                 collection_name=self.collection_name,
