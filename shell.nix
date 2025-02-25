0ae0f7389fd9ae7df1d34daf5a1ae9111d5f02ed
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-02-25 11:31:51 +0100

    Implement working fetching mechanism for Bible translations
    This commit introduces a functional mechanism to fetch all available
    Bible translations by scraping a specified webpage. It includes the
    creation of several new files: `fetch.py` for the downloading,
    extracting, and organizing of translations, along with configuration
    files like `.envrc` for environment setup, `.gitignore` to exclude
    certain file types, and `shell.nix` for Nix environment management. The
    `fetch.py` script implements a multi-step process: it scrapes the
    webpage for download links, downloads the zip files of translations,
    extracts their contents, filters for SQLite3 files, and finally
    organizes them by language into categorized directories. This structure
    will facilitate easier access and management of various Bible
    translations.

---
+++ shell.nix
@@ -0,0 +1,19 @@
+{ pkgs ? (import (builtins.fetchTarball { url =
+"https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz";
+}) {}) }:
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    sqlite
+    sqlite-vec
+    rlwrap
+
+    python3
+    python3Packages.requests
+    python3Packages.beautifulsoup4
+    python3Packages.lxml
+  ];
+  shellHook = ''
+    export SQLITE_VEC_PATH=${pkgs.sqlite-vec}/lib/vec.so
+  '';
+}
+
