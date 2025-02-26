c60ace768776833540018426020098ce9df25471
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-02-26 11:53:33 +0100

    Refactor merge.py for improved database merging and organization
    This commit refactors the merge.py script to enhance the process of
    merging databases into a single SQLite database. Key improvements
    include the creation of dedicated functions for each step of the merge
    process, such as creating source tables, copying books, verses,
    commentaries, and handling cross-references. By structuring the code
    this way, we improve maintainability and clarity. Additionally, an error
    handling mechanism has been introduced when copying words. The changes
    align with user story requirements aimed at streamlining the database
    merging functionality and ensuring data integrity throughout the
    operation.

--- merge.py
+++ merge.py
@@ -5,105 +5,91 @@ import re
 import glob
 import shutil
 
-LANGUAGE = sys.argv[1]
-MAIN = sys.argv[2]
-GROUPED_DIR = "grouped"
-MERGED_DIR = "merged"
+verses_view_sql = []
+commentaries_view_sql = []
 
-def merge_databases():
-    # Check if the merged directory exists
-    if not os.path.exists(MERGED_DIR):
-        os.mkdir(MERGED_DIR)
+def create_sources(cursor):
+    print("Creating _sources table...")
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS _sources (source_number NUM, name TEXT,
+        relation TEXT, description_short TEXT, description_long TEXT, origin
+        TEXT, chapter_string TEXT, chapter_string_ps TEXT, PRIMARY KEY
+        (source_number));
+    """)
+    cursor.execute("CREATE UNIQUE INDEX IF NOT EXISTS idx_sources ON _sources (source_number, name);")
 
-    lang_dir = os.path.join(GROUPED_DIR, LANGUAGE)
-    db_files = sorted(glob.glob(os.path.join(lang_dir, "*.SQLite3")))
-    merged_db_path = os.path.join(MERGED_DIR, f"{LANGUAGE}.SQLite3")
+def insert_source(cursor, index, name, relation):
+    cursor.execute(f"""
+        INSERT INTO _sources (
+            source_number, name, relation, description_short, description_long, 
+            origin, chapter_string, chapter_string_ps
+        )
+        SELECT 
+            {index} AS source_number,
+            '{name}' AS name,
+            '{relation}' AS relation,
+            (SELECT value FROM source.info WHERE name = 'description') AS description_short,
+            (SELECT value FROM source.info WHERE name = 'detailed_info') AS description_long,
+            (SELECT value FROM source.info WHERE name = 'origin') AS origin,
+            (SELECT value FROM source.info WHERE name = 'chapter_string') AS chapter_string,
+            (SELECT value FROM source.info WHERE name = 'chapter_string_ps') AS chapter_string_ps;
+    """)
+
+def copy_books(cursor):
+    print(f"- Copying books...")
+    cursor.execute(f"CREATE TABLE _books AS SELECT * FROM source.books_all;")
+
+def copy_verses(cursor, index, name):
+    print(f"- Copying verses...")
+    table_name = f"verses_{index:02d}"
+    cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.verses;")
+    verses_view_sql.append(f"SELECT '{index}' AS source_number, book_number, chapter, verse, text FROM {table_name}")
+    insert_source(cursor, index, name, table_name)
+
+def copy_commentaries(cursor, index, name):
+    print(f"- Copying commentaries...")
+    table_name = f"commentaries_{index:02d}"
+    cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.commentaries;")
+    commentaries_view_sql.append(f"SELECT '{index}' AS source_number, book_number, chapter_number_from, verse_number_from, chapter_number_to, verse_number_to, marker, text FROM {table_name}")
+    insert_source(cursor, index, name, table_name)
+
+def copy_dictionary(cursor, index, name):
+    print(f"- Copying dictionary...")
+    table_name = f"dictionary_{index:02d}"
+    cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.dictionary;")
+    insert_source(cursor, index, name, table_name)
+
+def copy_words(cursor, index, name):
+    print(f"- Copying words...")
+    table_name = f"words_{index:02d}"
+    cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.words;")
+    insert_source(cursor, index, name, table_name)
+
+def process_source(cursor, index, main_db_path, db_path):
+    name = os.path.basename(db_path).replace(".SQLite3", "")
+    print(f"Processing: {name}")
+    cursor.execute(f"ATTACH '{db_path}' AS source;")
+    if db_path == main_db_path:
+        copy_books(cursor)
+    if re.search(r"\.commentaries\.SQLite3$", db_path):
+        copy_commentaries(cursor, index, name)
+    elif re.search(r"\.dictionary\.SQLite3$", db_path):
+        copy_dictionary(cursor, index, name)
+        try:
+            copy_words(cursor, index, name)
+        except:
+            print(f"                  ...no words found!")
+    else:
+        copy_verses(cursor, index, name)
+    cursor.execute("COMMIT;")
+    cursor.execute("DETACH source;")
+
+def copy_cross_references(cursor):
+    print(f"Copying _all_references...")
     references_db_path = "cross_references.SQLite3"
-
-    # Remove previous merged database if exists
-    if os.path.exists(merged_db_path):
-        os.remove(merged_db_path)
-
-    # Use the "main" database
-    main_db = os.path.join(lang_dir, f"{MAIN}.SQLite3")
-
-    print(f"Main DB: {main_db}")
-    print(f"Merging {len(db_files)} databases into {merged_db_path}")
-
-    # Connect to new merged database
-    merged_conn = sqlite3.connect(merged_db_path)
-    merged_cursor = merged_conn.cursor()
-
-    # Disable foreign keys (to avoid issues)
-    merged_cursor.execute("PRAGMA foreign_keys=OFF;")
-
-    # Create tables
-    merged_cursor.execute("CREATE TABLE IF NOT EXISTS _info (source_number NUM, name TEXT, value TEXT);")
-    merged_cursor.execute("CREATE TABLE IF NOT EXISTS _sources (source_number NUM, name TEXT, relation TEXT);")
-    merged_cursor.execute("CREATE TABLE IF NOT EXISTS _all_references (address TEXT, address_from TEXT, address_to TEXT, rate NUM);")
-
-    verses_view_sql = []
-    commentaries_view_sql = []
-
-    for index, db_path in enumerate(db_files):
-        name = os.path.basename(db_path).replace(".SQLite3", "").replace("'", "@")
-        os.rename(db_path, os.path.join(lang_dir, f"{name}.SQLite3"))
-        db_path = os.path.join(lang_dir, f"{name}.SQLite3")
-
-        print(f"Processing: {name}")
-
-        # Attach database
-        merged_cursor.execute(f"ATTACH '{db_path}' AS source;")
-
-        # Copy info table
-        print(f"- Copying info from {name}...")
-        merged_cursor.execute(f"INSERT INTO _info SELECT {index}, name, value FROM source.info;")
-
-        # If it's the main database, copy books
-        if db_path == main_db:
-            print(f"- Copying books (from main database: {MAIN})...")
-            merged_cursor.execute(f"CREATE TABLE _books AS SELECT * FROM source.books_all;")
-
-        # if db_path matches "*.commentaries.SQLite3"
-        if re.search(r"\.commentaries\.SQLite3$", db_path):
-            # Copy commentaries
-            table_name = f"commentaries_{index:02d}"
-            print(f"- Copying commentaries from {name}...")
-            merged_cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.commentaries;")
-            commentaries_view_sql.append(f"SELECT '{index}' AS source_number, book_number, chapter_number_from, verse_number_from, chapter_number_to, verse_number_to, marker, text FROM {table_name}")
-            merged_cursor.execute(f"INSERT INTO _sources (source_number, name, relation) VALUES ({index}, '{name}', '{table_name}');")
-        elif re.search(r"\.dictionary\.SQLite3$", db_path):
-            # Copy dictionary
-            table_name = f"dictionary_{index:02d}"
-            print(f"- Copying dictionary from {name}...")
-            merged_cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.dictionary;")
-            merged_cursor.execute(f"INSERT INTO _sources (source_number, name, relation) VALUES ({index}, '{name}', '{table_name}');")
-            # Copy words
-            try:
-                table_name = f"words_{index:02d}"
-                print(f"- Copying words from {name}...")
-                merged_cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.words;")
-                merged_cursor.execute(f"INSERT INTO _sources (source_number, name, relation) VALUES ({index}, '{name}', '{table_name}');")
-            except:
-                print(f"- No words table found in {name}")
-        else:
-            # Copy verses
-            table_name = f"verses_{index:02d}"
-            print(f"- Copying verses from {name}...")
-            merged_cursor.execute(f"CREATE TABLE {table_name} AS SELECT * FROM source.verses;")
-            verses_view_sql.append(f"SELECT '{index}' AS source_number, book_number, chapter, verse, text FROM {table_name}")
-            merged_cursor.execute(f"INSERT INTO _sources (source_number, name, relation) VALUES ({index}, '{name}', '{table_name}');")
-
-        # Commit changes before detaching
-        merged_cursor.execute("COMMIT;")
-
-        # Detach database
-        merged_cursor.execute("DETACH source;")
-
-    # Copy cross_references
-    print(f"Copying cross_references...")
-    merged_cursor.execute(f"ATTACH '{references_db_path}' AS ref;")
-    merged_cursor.execute(f"""
+    cursor.execute("CREATE TABLE IF NOT EXISTS _all_references (address TEXT, address_from TEXT, address_to TEXT, rate NUM);")
+    cursor.execute(f"ATTACH '{references_db_path}' AS ref;")
+    cursor.execute("""
         INSERT INTO _all_references
         SELECT (b.short_name || ' ' || r.chapter || ',' || r.verse) AS address,
                (b1.short_name || ' ' || r.c1 || ',' || r.v1) AS address_from,
@@ -114,11 +100,12 @@ def merge_databases():
         JOIN _books b1 ON r.b1 = b1.book_number
         JOIN _books b2 ON r.b2 = b2.book_number;
     """)
-    merged_cursor.execute("COMMIT;")
-    merged_cursor.execute("DETACH ref;")
+    cursor.execute("COMMIT;")
+    cursor.execute("DETACH ref;")
 
-    # Create view combining all verses
-    merged_cursor.execute(f"""
+def create_all_verses_view(cursor):
+    print("Creating view _all_verses...")
+    cursor.execute(f"""
         CREATE VIEW _all_verses AS
         SELECT v.*,
                (b.short_name || ' ' || v.chapter || ',' || v.verse) AS address,
@@ -127,9 +114,11 @@ def merge_databases():
         JOIN _sources s ON v.source_number = s.source_number
         JOIN _books b ON v.book_number = b.book_number;
     """)
-    # Create view combining all commentaries
+
+def create_all_commentaries_view(cursor):
     if commentaries_view_sql:
-        merged_cursor.execute(f"""
+        print("Creating view _all_commentaries...")
+        cursor.execute(f"""
             CREATE VIEW _all_commentaries AS 
             SELECT c.*, 
                    (b.short_name || ' ' || c.chapter_number_from || ',' || c.verse_number_from) AS address_from,
@@ -138,10 +127,38 @@ def merge_databases():
             JOIN _books b ON c.book_number = b.book_number;
         """)
 
-    # Commit and close
-    merged_conn.commit()
-    merged_conn.close()
+def rename_db(db_path):
+    os.rename(db_path, db_path.replace("'", "@"))
+    return db_path.replace("'", "@")
+
+def merge_databases(merged_db_path, main_db_path, db_files):
+    if os.path.exists(merged_db_path):
+        os.remove(merged_db_path)
+    conn = sqlite3.connect(merged_db_path)
+    cursor = conn.cursor()
+    cursor.execute("PRAGMA foreign_keys=OFF;")
+    create_sources(cursor)
+    for index, db_path in enumerate(db_files):
+        process_source(cursor, index, main_db_path, rename_db(db_path))
+    copy_cross_references(cursor)
+    create_all_verses_view(cursor)
+    create_all_commentaries_view(cursor)
+    conn.commit()
+    conn.close()
     print(f"Merged database created: {merged_db_path}")
 
 if __name__ == "__main__":
-    merge_databases()
+    LANGUAGE = sys.argv[1]
+    MAIN = sys.argv[2]
+    GROUPED_DIR = "grouped"
+    MERGED_DIR = "merged"
+
+    if not os.path.exists(MERGED_DIR):
+        os.mkdir(MERGED_DIR)
+
+    lang_dir = os.path.join(GROUPED_DIR, LANGUAGE)
+    db_files = sorted(glob.glob(os.path.join(lang_dir, "*.SQLite3")))
+    merged_db_path = os.path.join(MERGED_DIR, f"{LANGUAGE}.SQLite3")
+    main_db_path = os.path.join(lang_dir, f"{MAIN}.SQLite3")
+
+    merge_databases(merged_db_path, main_db_path, db_files)
