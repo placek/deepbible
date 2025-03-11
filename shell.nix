c9517dc90f90e5d59f92fdd9df0fcd83f92b2393
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-03-11 13:16:41 +0100

    Reorganize project structure by moving scripts to a dedicated tools directory
    This commit refactors the project structure to improve organization and
    clarity. The primary change involves moving several script files related
    to project functionality into a newly created `tools` directory,
    including `embed.py`, `fetch.py`, `generate.py`, and `merge.py`.
    Furthermore, the directory paths for merged data and other resources
    have been updated to utilize a `data` subdirectory for better file
    management. We also removed the unused `address.py` file and deleted the
    old database file `cross_references.SQLite3`. The shell environment
    configuration in `shell.nix` has been updated to include `sqlitebrowser`
    in the build inputs.
    
    Changes:
    - Removed `address.py` and `cross_references.SQLite3`
    - Renamed and moved `embed.py`, `fetch.py`, `generate.py`, and
      `merge.py` to `tools/`
    - Updated directory paths within scripts to refer to `data/merged`,
      `data/downloads`, `data/extracted`, and `data/grouped`
    - Added `sqlitebrowser` to the build inputs in `shell.nix`

--- shell.nix
+++ shell.nix
@@ -1,11 +1,11 @@
-{ pkgs ? (import (builtins.fetchTarball { url =
-"https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz";
-}) {}) }:
+{ pkgs ? (import (builtins.fetchTarball { url = "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz"; }) {})
+}:
 pkgs.mkShell {
   buildInputs = with pkgs; [
     sqlite
     sqlite-vec
     rlwrap
+    sqlitebrowser
 
     python3
     python3Packages.requests
