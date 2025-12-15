a1ca439b214f04c1a42e91a8e72eef54f2b4eb97
Paweł Placzyński <placzynski.pawel@gmail.com>
2025-12-15 15:20:31 +0100

    Use <nixpkgs> import in shell.nix
    Simplify shell.nix by replacing the explicit fetchTarball import with `<nixpkgs>`, leveraging local Nix channels and reducing maintenance overhead

--- shell.nix
+++ shell.nix
@@ -1,6 +1,4 @@
-{ pkgs ? (import (builtins.fetchTarball {
-    url = "https://github.com/NixOS/nixpkgs/archive/21808d22b1cda1898b71cf1a1beb524a97add2c4.tar.gz";
-  }) {})
+{ pkgs ? (import <nixpkgs> {})
 }:
 let
   system = pkgs.stdenv.hostPlatform.system;
